57d78ba46ba1f7eec97f01a2c1f1f6ee7601aceb
Ayaka <layakas030@gmail.com>
2026-09-24 16:42:40 +0800

    前往頁面完成

---
+++ project1/src/App.vue
@@ -0,0 +1,19 @@
+
+
+
+<template>
+  <div class="app">
+    <Go/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Go from './components/Go.vue';
+
+    export default{
+        name: 'App',
+        components: {Go}
+    }
+</script>
+
+<style scoped></style>
---
+++ project1/src/components/Go.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex h-screen items-center justify-center p-4  bg-black/20">
+        <div class="w-full max-w-88.5 min-h-29 font-bold bg-white rounded-2xl shadow-[0_4px_16px_rgb(0,0,0,0.3)]">
+            <div class="grid place-items-center h-1/2 p-4">
+                    <p class="text-xl">前往聯邦銀行</p>
+            </div>
+            <a href="#" class="grid place-items-center h-1/2 p-4 border-t border-gray-200">
+                使用網站
+            </a>
+        </div>
+    </div>
+</template>
+<script lang="ts"></script>
+<style scoped></style>
